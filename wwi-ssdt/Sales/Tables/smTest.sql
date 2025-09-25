37759052497ff0d6dd4a42b897170f8afb63c173
smartz76 <smartz76@users.noreply.github.com>
2025-09-25 15:40:54 -0500

    created a new test table for sales schema in wwi-ssdt project

---
+++ wwi-ssdt/Sales/Tables/smTest.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [Sales].[smTest] (
+    [TestID]   INT           NOT NULL,
+    [Description] NVARCHAR (50) NOT NULL,
+    [LastEditedBy]    INT           NOT NULL,
+    [ValidFrom]       DATETIME2 (7) NOT NULL,
+    [ValidTo]         DATETIME2 (7) NOT NULL
+);
